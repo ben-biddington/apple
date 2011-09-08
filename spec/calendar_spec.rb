1262c2d5a14929f5acc01d05324aef8af4be49de
Ben Biddington <ben.biddington@gmail.com>
2011-09-09 11:40:49 +1200

    Moved the calendar into the project

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,12 +1,21 @@
 require "spec_helper"
 
 describe "The calendar on the split apple rock site" do 
+  before :all do
+    @base_url = "file:///home/ben/sauce/split_apple_rock/calendar.html"
+  end
+
   it "shows the correct timezone" do
-    visit "file:///home/ben/sauce/google_calendar/example.html"
+    visit @base_url
+    page.should have_content "Time zone offset (hours): -12"
+  end
+
+  it "shows the correct days for whatever month is requested" do
+    visit "#{@base_url}?y=2011&m=sep"
     page.should have_content "Time zone offset (hours): -12"
   end
 
   it "shows a different calendar by setting month and/or year query parameters"
-  it "shows the correct days for whatever month is requested"
+
   it "shows all the the calendar items for the requested month"
 end
