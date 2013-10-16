d7271d58f50536fdd069f7f3b38fe51dbab08752
Ben Biddington <ben.biddington@gmail.com>
2013-10-16 13:27:54 +1300

    A reasonable home page

---
+++ rake/pictures.rake.rb
@@ -0,0 +1,28 @@
+desc "Resize an image"
+task :resize do
+  source_dir = File.expand_path(File.join ".", "pictures")
+  
+  Image.new("#{source_dir}/IMG_2477.JPG").tap do |img|
+    img.on(:resized){|e,args| puts args.first}
+  end.resize(:percentage => 40, :to => "next/img/home.jpg")
+end
+
+class Image
+  require "audible"; include Audible
+
+  def initialize(name)
+    @name = name
+  end
+
+  def resize(opts = {})
+    percentage = opts[:percentage] || 50
+    
+    output = opts[:to] || fail("No :to supplied, don't know where to save it to.")
+
+    fail "I refuse to overwrite" if output === @name
+
+    `convert #{@name} -resize #{percentage}% #{output}`
+
+    notify :resized, "Resized <#{@name}> by #{percentage}% and wrote to <#{output}>"
+  end
+end
